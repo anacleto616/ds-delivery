ec71b1d2097f0ecd763f538f1ea90c6a6d01c165
Rafael Anacleto <rafael.anacleto12@gmail.com>
2021-01-14 21:18:31 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,21 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate("Home");
+  };
   return (
-    <View style={styles.container}>
-      <Image source={require("../assets/logo.png")} />
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require("../assets/logo.png")} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -25,7 +34,7 @@ const styles = StyleSheet.create({
     letterSpacing: -0.24,
     color: "#FFF",
     marginLeft: 15,
-    fontFamily: 'OpenSans_700Bold'
+    fontFamily: "OpenSans_700Bold",
   },
 });
 
